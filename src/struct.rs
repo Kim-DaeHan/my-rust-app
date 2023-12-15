5ea5dcf5778eeb50eca7f7aa5ceee5ede727b916
Kim-DaeHan <Kim-DaeHan@users.noreply.github.com>
2023-12-15 18:49:44 +0900

    .

--- src/struct.rs
+++ src/struct.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     length: u32,
     width: u32,
@@ -9,12 +10,17 @@ fn main() {
         width: 30,
     };
 
+    // :? 명시자를 집어넣는 것은 println! 에게 Debug라 불리우는 출력 포맷을 사용하고 싶다고 말해줌(구조체 정의 부분 바로 전에 #[derive(Debug)] 넣어줘야함)
+    println!("rect1 is {:?}", rect1);
+    // 좀 더 이쁘게 출력 {:#?}
+    println!("rect1 is {:#?}", rect1);
+
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
 }
